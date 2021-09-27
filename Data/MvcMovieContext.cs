cdc62b2b553098f726bc9e14e9b84671596540dc
LINH CHI HOANG THI <1821050363@student.humg.edu.vn>
2021-09-27 10:08:57 +0700

    UpLoad File

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Demo.Models;
+
+namespace Demo.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Demo.Models.Movie> Movie { get; set; }
+        public DbSet<Student> Student{ get; set; }
+ 
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage="Titile is requaired")]
+        [MaxLength(15)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
